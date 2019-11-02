730fe20931b285c22113875174356d685655b02a
Koutawars <admikouta@gmail.com>
2019-11-02 17:37:43 -0500

    Proyecto base con reactJS y NodeJS

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express'); // Importa la libreria express
+var app = express(); // ejecuta la librería
+var path = require('path'); // esta librería la trae node incluida, acá se require
+var body_parser = require('body-parser'); 
+
+app.set('views', path.join(__dirname ,'/views'));
+
+app.use(body_parser.urlencoded({extended:true}));
+
+app.use('/', express.static(path.resolve(__dirname + '/views/build/')));
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname + '/views/build/index.html'));
+});
+app.listen(process.env.PORT || 5000, function () {
+    console.log('escuchando en '+ (process.env.PORT || 5000));
+});
