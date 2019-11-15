269fe2e68773f4a93abc009466d5d90a4f01b1ad
Koutawars <admikouta@gmail.com>
2019-11-14 23:23:17 -0500

    XD

--- config/email.js
+++ config/email.js
@@ -2,6 +2,9 @@
 // testchan1234
 
 var nodemailer = require('nodemailer');
+var inlineBase64 = require('nodemailer-plugin-inline-base64');
+
+
 var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
@@ -9,14 +12,16 @@ var transporter = nodemailer.createTransport({
         pass: 'testchan1234'
     }
 });
-exports.enviarEmail = function(correo, image){
+transporter.use('compile', inlineBase64({cidPrefix: 'somePrefix_'}));
+
+exports.enviarEmail = function(correo, image, clase){
+    let html = `<html><body><h4>CODEQR</h4><img src="${image}"></body></html>`;
     var mailOptions = {
         from: 'testchan.asistenciauwu@gmail.com',
         to: correo,
-        subject: 'QrCode clase asistencia',
-        text: 'Imagen: ',
-        html: '<img src = "' + image + '" />' 
-    };
+        subject: 'QrCode clase #' + clase,
+        html
+    }
     // Enviamos el email
     transporter.sendMail(mailOptions, function(error, info){
         if (error){
